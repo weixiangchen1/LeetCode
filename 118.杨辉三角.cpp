4bedb9f35b03935f5179faba9de039ff051b3d3e
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2022-05-14 21:30:58 +0800

    Add files via upload

---
+++ 118.杨辉三角.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> vv;
+        vv.resize(numRows);
+        for(int i=0; i<numRows; ++i)
+        {
+            vv[i].resize(i+1);
+            vv[i][0] = 1;
+            vv[i][vv[i].size()-1] = 1;
+        }
+
+        for(int i=0; i<numRows; ++i)
+        {
+            for(int j=0; j<vv[i].size(); ++j)
+            {
+                if(vv[i][j]==0)
+                {
+                    vv[i][j] = vv[i-1][j] + vv[i-1][j-1];
+                }
+            }
+        }
+
+        return vv;
+
+    }
+};
