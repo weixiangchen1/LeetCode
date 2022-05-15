fdbefd26960075792aee036193a6219536ed15ba
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2022-05-15 19:41:54 +0800

    Add files via upload

---
+++ 100.相同的树.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q){
+    if(q == NULL && p == NULL)
+        return true;
+
+    if(q == NULL || p == NULL)
+        return false;
+    
+    if(q->val != p->val)
+        return false;
+
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
