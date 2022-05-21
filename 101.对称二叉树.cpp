c116c36e1d532726e7afbe0807229a7c049c61c9
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2022-05-21 23:07:03 +0800

    Add files via upload

---
+++ 101.对称二叉树.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool _isSymmetric(struct TreeNode* left, struct TreeNode* right)
+{
+    if(!left && !right)
+        return true;
+    if(!left || !right)
+        return false;
+    if(left->val != right->val)
+        return false;
+
+    return _isSymmetric(left->left, right->right) && _isSymmetric(left->right, right->left);
+}
+
+bool isSymmetric(struct TreeNode* root){
+    if(root == NULL)
+        return true;
+    
+
+    return _isSymmetric(root->left, root->right);
+}
