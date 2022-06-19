2ba6e798f46cacae4aa08cea9b1d98db80d18546
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2022-06-19 22:24:13 +0800

    Add files via upload

---
+++ 203.移除链表元素.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+    struct ListNode* cur = head;
+    struct ListNode* prev = NULL;
+
+    while(cur)
+    {
+        if(cur->val == val)
+        {
+            if(cur == head)
+            {
+                head = cur->next;
+                free(cur);
+                cur = head;
+            }
+            else
+            {
+                prev->next = cur->next;
+                free(cur);
+                cur = prev->next;
+            }
+        }
+        else
+        {
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+
+    return head;
+}
