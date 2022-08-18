86dd8a1ec044ac99417da5e8eb4af45a73bb0664
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2022-08-18 16:25:31 +0800

    Add files via upload

---
+++ 有序数组的插入.cpp
@@ -0,0 +1,16 @@
+bool Insert( List L, ElementType X )
+{
+	int i, j;
+	for(i=0;i<=L->Last;i++)
+	{
+		if(L->Data[i]==X) 
+          return false;
+		else if(L->Data[i]<X)
+		  break;
+	}
+	for(j=L->Last;j>=i;j--)
+	   L->Data[j+1]=L->Data[j];
+	L->Data[i]=X;
+    L->Last++;
+	return true;
+}
