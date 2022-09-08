8498cb0776e0d08e3ccd1b576481c71c4d3bbbad
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2022-09-08 22:32:52 +0800

    Add files via upload

---
+++ 144.二叉树的前序遍历(非递归).cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        vector<int> ans;
+
+        while(cur || !st.empty())
+        {
+            while(cur)
+            {
+                st.push(cur);
+                ans.push_back(cur->val);
+                cur = cur->left;
+            }
+
+            TreeNode* top = st.top();
+            st.pop();
+
+            cur = top->right;
+        }
+
+        return ans;
+    }
+};
