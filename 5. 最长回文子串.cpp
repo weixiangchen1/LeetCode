66ffc6d140ded0f920de88a6dbf058400e602d6e
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2022-11-16 17:30:35 +0800

    Add files via upload

---
+++ 5. 最长回文子串.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res;
+        for (int i = 0; i < s.size(); i++) {
+            int left = i-1, right = i+1;
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            if (res.size() < right - left - 1) {
+                res = s.substr(left + 1, right - left - 1);
+            }
+
+            left = i, right = i+1;
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            if (res.size() < right - left - 1) {
+                res = s.substr(left + 1, right - left - 1);
+            }
+        }
+
+        return res;
+    }
+};
