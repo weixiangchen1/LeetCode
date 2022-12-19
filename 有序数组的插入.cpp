d6add8531506924f3a8050bb249be7838daf953e
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2022-12-19 23:13:41 +0800

    updete

--- 有序数组的插入.cpp
+++ 有序数组的插入.cpp
@@ -3,14 +3,17 @@ bool Insert( List L, ElementType X )
 	int i, j;
 	for(i=0;i<=L->Last;i++)
 	{
-		if(L->Data[i]==X) 
-          return false;
-		else if(L->Data[i]<X)
-		  break;
+	    if(L->Data[i]==X) 
+               return false;
+	    else if(L->Data[i]<X)
+	       break;
 	}
 	for(j=L->Last;j>=i;j--)
-	   L->Data[j+1]=L->Data[j];
+        {
+           L->Data[j+1]=L->Data[j];
+        }
+	   
 	L->Data[i]=X;
-    L->Last++;
+        L->Last++;
 	return true;
 }
