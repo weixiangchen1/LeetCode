4c90ac697ea357adf051a0f3fd2e4117ac8ed3ad
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2023-03-20 21:59:45 +0800

    LeetCode No.56

---
+++ 56. 合并区间.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+		vector<vector<int>> ans;
+		if (intervals.size() == 0) return ans;
+
+		sort(intervals.begin(), intervals.end());
+		int start = -1, end = -1;
+		for (auto interval : intervals)
+		{
+			if (end < interval[0])
+			{
+				if (end != -1) ans.push_back({ start, end });
+				start = interval[0];
+				end = interval[1];
+			}
+			else
+			{
+				end = max(end, interval[1]);
+			}
+		}
+
+		if (end != -1) ans.push_back({ start, end });
+
+		return ans;
+	}
+};
