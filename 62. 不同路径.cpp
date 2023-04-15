cd7c1223c38e0ea85274237dd288fda921948087
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2023-04-15 20:54:02 +0800

    LeetCode No.62

---
+++ 62. 不同路径.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+		vector<vector<int>> dp(m, vector<int>(n));
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (i == 0 && j == 0) {
+					// 起点
+					dp[i][j] = 1;
+				}
+				else {
+					if (i != 0) {
+						// 表示可以从上面向下走
+						dp[i][j] += dp[i - 1][j];
+					}
+					if (j != 0) {
+						// 表示可以从左面往右走
+						dp[i][j] += dp[i][j - 1];
+					}
+				}
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+};
