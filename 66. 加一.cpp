828461460674cc5bd123c7a6e6aee4cd2bd676e8
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2023-05-08 22:38:36 +0800

    LeetCode No.66

--- 66. 加一.cpp
+++ 66. 加一.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-	vector<int> plusOne(vector<int>& digits) {
-		vector<int> ans(digits);
-		ans[ans.size() - 1]++;
-		if (ans[ans.size() - 1] == 10) {
-			ans[ans.size() - 2]++;
-			ans.push_back(0);
-		}
-		return ans;
-	}
+    vector<int> plusOne(vector<int>& digits) {
+        int tmp = 1;
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            digits[i] += tmp;
+            tmp = 0;
+            if (digits[i] > 9) {
+                tmp = digits[i] / 10;
+                digits[i] %= 10;
+            } 
+        }
+        if (tmp != 0) digits.insert(digits.begin(), 1);
+
+        return digits;
+    }
 };
