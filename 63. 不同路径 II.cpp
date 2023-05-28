82af8699debffef6e152b77ee5fbec82bb0da43f
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2023-05-28 17:07:08 +0800

    LeetCode No.63

---
+++ 63. 不同路径 II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+		if (obstacleGrid[0][0] == 1) return 0;
+		int n = obstacleGrid.size();
+		int m = obstacleGrid[0].size();
+		vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+		dp[0][1] = 1;
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				if (obstacleGrid[i - 1][j - 1] == 1) dp[i][j] = 0;
+				else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+		return dp[n][m];
+	}
+};
